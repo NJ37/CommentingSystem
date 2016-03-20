9b6255cc55386bd6480f571424322b92903f498d
NJ37 <ep13b018@smail.iitm.ac.in>
2016-03-21 00:11:21 +0530

    Update ang.js
    Fixed bugs to display persistent data and also update comments, upvotes directly to the database also.

--- public/javascripts/ang.js
+++ public/javascripts/ang.js
@@ -48,7 +48,7 @@ function($stateProvider, $urlRouterProvider) {
   		templateUrl: '/home.html',
   		controller: 'Base',
   		resolve: {
-    		comment: ['comments', function(comments){ //using resolve property of ui-router - not rendering???
+    		postPromise: ['comments', function(comments){
       			return comments.getAll();
     	}]
   	}
@@ -59,35 +59,22 @@ function($stateProvider, $urlRouterProvider) {
 
 app.controller('Base',[
 	'$scope','comments',function($scope,comments){
-		$scope.addComment=function(){ //add new comments to the server
-			$scope.comments=comments.comments;
+      $scope.comments=comments.comments;
+		$scope.addComment=function(){
 			if(!$scope.username||$scope.username==''){$scope.username='Anonymous';}
 			if(!$scope.contents||$scope.contents==''){return;}
-			// comments.create({
-   //  			username: $scope.username,
-   //  			contents: $scope.contents,
-  	// 			});
-			$scope.comments.push({username:$scope.username,contents:$scope.contents,upvotes:0,downvotes:0});
+			comments.create({
+    			username: $scope.username,
+    			contents: $scope.contents,
+  				});
 			$scope.username='';
 			$scope.contents='';
 		}
-		$scope.increaseUpvotes = function(comment) {  //function which updates the upvotes to the database
-  			comments.upvote(comment);
-		}
-		//  $scope.increaseUpvotes=function(comment){  //function which increases upvotes in the page
-		//  	comment.upvotes+=1;
-		// }
-		$scope.increaseDownvotes=function(comment){   //function which updates the downvotes to the database
-			comment.downvotes+=1;
-		}
-		// $scope.increaseDownvotes=function(comment){  //function which increases downvotes in the page
-		// 	comment.downvotes+=1;
-		// }
-$scope.comments = [
-  {username: 'Diana', contents:'In either a quantum world or in a higher dimension, the past, present and future co-exist!', upvotes: 5, downvotes:0},
-  {username: 'Stella', contents:'If Betelgeuse would explode transiting from the red super giant stage to supernova then our sky would light continuously for two months. It can happen anytime, within a couple of thousand years, tomorrow or even now!', upvotes: 9, downvotes:1},
-  {username: 'Jake', contents:'One species of jellyfish, Turritopsis nutricula, are immortal.', upvotes: 6, downvotes:2},
-  {username: 'Cindy', contents:'Never wash strawberries or any berry unless you intend to eat them right away or they will mold.', upvotes: 7, downvotes:0},
-  {username: 'Michael', contents:'The H-bomb is the bloodless weapon. It leaves only radioactive ash in a 20km radius.', upvotes: 6, downvotes:3}
-];
+$scope.increaseUpvotes = function(comment) {
+  comments.upvote(comment);
+}
+$scope.decreaseUpvotes = function(comment) {
+  comments.downvote(comment);
+}
+
 	}]);
